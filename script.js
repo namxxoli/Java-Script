8b8a7e95cf977e3dbbf2efc4527c576ed4c55b7e
namxxoli <115549556+namxxoli@users.noreply.github.com>
2022-12-08 20:55:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let name = "Inês";
+let age = 18;
+console.log(name,age)
+
+age = 126; 
+
+let friends = ["Guilherme", "Eva", "Ricardo"];
+let numbers = [1, 2, 3];
+console.log(friends[2]);
+// Inês
+
+let person = {
+        name: "Guilherme",
+        age: 18,
+        isStudent: true
+    };
+    console.log(person.name);
+// Inês
+
+let people = [
+    {
+        name: "Guilherme",
+        age: 18,
+        isStudent: true
+    },
+    {
+        name: "Eva",
+        age: 18,
+        isStudent: true
+    },
+    {
+        name: "Ricardo",
+        age: 19,
+        isStudent: true
+    }
+]
+    console.log(people[2].name);
+// Inês
